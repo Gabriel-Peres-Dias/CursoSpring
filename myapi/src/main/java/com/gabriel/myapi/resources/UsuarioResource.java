3ae2dc39dd0eda953b8cfc47d643df366730ce6a
Gabriel Peres Dias <gabriel.suporte123@gmail.com>
2022-08-25 19:38:43 -0300

    metodo save

--- myapi/src/main/java/com/gabriel/myapi/resources/UsuarioResource.java
+++ myapi/src/main/java/com/gabriel/myapi/resources/UsuarioResource.java
@@ -1,14 +1,17 @@
 package com.gabriel.myapi.resources;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.myapi.domain.Usuario;
 import com.gabriel.myapi.services.UsuarioService;
@@ -36,6 +39,13 @@ public class UsuarioResource {
 	public ResponseEntity<Usuario>update(@PathVariable Integer id, @RequestBody Usuario obj){
 		Usuario newObj = service.update(id, obj);
 		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> save(@RequestBody Usuario obj){
+		Usuario newObj = service.save(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 	
